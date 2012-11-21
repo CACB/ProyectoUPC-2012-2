a23e31e7239e15bfd2b7122fe47724d1f36a865b
silvina <silvinabecerrav@gmail.com>
2012-11-21 00:33:58 -0500

    Agregando Test para la clase AdmVentas

--- test/AdmVentasTest.java
+++ test/AdmVentasTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,10 +8,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author dv2000
- */
 public class AdmVentasTest {
     
     public AdmVentasTest() {
@@ -38,75 +29,7 @@ public class AdmVentasTest {
     public void tearDown() {
     }
 
-//    /**
-//     * Test of fn_getDoc method, of class AdmVentas.
-//     */
-//    @Test
-//    public void testFn_getDoc() {
-//        System.out.println("fn_getDoc");
-//        AdmVentas instance = new AdmVentas();
-//        ArrayList expResult = null;
-//        ArrayList result = instance.fn_getDoc();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of validarDatosIncompletos method, of class AdmVentas.
-//     */
-//    @Test
-//    public void testValidarDatosIncompletos() throws Exception {
-//        System.out.println("validarDatosIncompletos");
-//        String numero = "";
-//        String fecha_emision = "";
-//        String fecha_vencimiento = "";
-//        String empresa = "";
-//        String fecha_pago = "";
-//        String estado = "";
-//        String concepto = "";
-//        double subtotal = 0.0;
-//        double igv = 0.0;
-//        double total = 0.0;
-//        String moneda = "";
-//        AdmVentas instance = new AdmVentas();
-//        instance.validarDatosIncompletos(numero, fecha_emision, fecha_vencimiento, empresa, fecha_pago, estado, concepto, subtotal, igv, total, moneda);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of Fn_buscar method, of class AdmVentas.
-//     */
-//    @Test
-//    public void testFn_buscar() {
-//        System.out.println("Fn_buscar");
-//        String numero = "";
-//        AdmVentas instance = new AdmVentas();
-//        Ventas expResult = null;
-//        Ventas result = instance.Fn_buscar(numero);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of validarDuplicidad method, of class AdmVentas.
-//     */
-//    @Test
-//    public void testValidarDuplicidad() throws Exception {
-//        System.out.println("validarDuplicidad");
-//        String numero = "";
-//        AdmVentas instance = new AdmVentas();
-//        instance.validarDuplicidad(numero);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-
-    /**
-     * Test of registrarDocumento method, of class AdmVentas.
-     */
-    @Test
+   @Test
     public void testRegistrarDocumento() {
         System.out.println("registrarDocumento");
         String numero = "666";
@@ -132,42 +55,10 @@ public class AdmVentasTest {
         }
         
         // TODO review the generated test code and remove the default call to fail.
-         Ventas oVentas = oAdmVentas.Fn_buscar(numero);
+        Ventas oVentas = oAdmVentas.Fn_buscar_nro_documento(numero);
         assertNotNull(oVentas);
-
     }
-    
-//    @Test
-//    public void DebeRegistrarHotelValidandoDatosIngresadosCompletos() {
-//
-//        //Arrange
-//        String S_ctacorreo = "admin@marriot.com.pe";
-//        String S_contrasenax = "123456";
-//        String S_contrasenay = "123456";
-//        String S_nombre = "Hotel_Marriot";
-//        String S_direccion = "http://Hotel_Marriot.clerk.im";
-//        boolean B_aceptartermino = true;
-//        int I_canthabitacion = 0;
-//
-//        Ctr_hoteladmin obj_Ctr_hoteladmin = new Ctr_hoteladmin();
-//        try {
-//            //Act
-//            obj_Ctr_hoteladmin.Fn_registrarhotel(S_ctacorreo, S_contrasenax, S_contrasenay, S_nombre, S_direccion, B_aceptartermino, I_canthabitacion);
-//            //Assert
-//            // Espero que ocurra un error
-//        } catch (BusinessException ex) {
-//            Logger.getLogger(HotelAdminTest.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//
-//        //Assert
-//        Ent_hotel objhotel = obj_Ctr_hoteladmin.Fn_buscarnombre(S_nombre);
-//        assertNotNull(objhotel);
-//
-//    }
-//
-//    /**
-//     * Test of eliminarDocumento method, of class AdmVentas.
-//     */
+
     @Test
     public void testEliminarDocumento() throws Exception {
         System.out.println("registrarDocumento");
@@ -194,21 +85,18 @@ public class AdmVentasTest {
         }
         
         // TODO review the generated test code and remove the default call to fail.
-        Ventas oVentas = oAdmVentas.Fn_buscar(numero);
+        Ventas oVentas = oAdmVentas.Fn_buscar_nro_documento(numero);
         assertNotNull(oVentas);
         
         oAdmVentas.eliminarDocumento(numero);
-        oVentas = oAdmVentas.Fn_buscar(numero);
+        oVentas = oAdmVentas.Fn_buscar_nro_documento(numero);
         assertNull(oVentas);
     }
 
-    /**
-     * Test of editarDocumento method, of class AdmVentas.
-     */
     @Test
     public void testEditarDocumento() throws Exception {
         System.out.println("editarDocumento");
-        String numero = "666";
+        String numero = "666";       
         String fecha_emision = "16/06/2012";
         String fecha_vencimiento = "16/06/2012";
         String empresa = "Peru";
@@ -231,11 +119,68 @@ public class AdmVentasTest {
         }
         
         // TODO review the generated test code and remove the default call to fail.
-         Ventas oVentas = oAdmVentas.Fn_buscar(numero);
-        assertNotNull(oVentas);
+         Ventas oVentas = oAdmVentas.Fn_buscar_nro_documento(numero);
+        assertNotNull(oVentas);        
         
         
+        fecha_emision = "17/07/2012";
+        fecha_vencimiento = "17/07/2012";
+        empresa = "Peru";
+        fecha_pago = "17/07/2012";
+        estado = "1";
+        concepto = "C#";
+        subtotal = 200.0;
+        igv = 38.0;
+        total = 238.0;
+        moneda = "ME";        
         
-
+        oAdmVentas.editarDocumento(numero, fecha_emision, fecha_vencimiento, empresa, fecha_pago, estado, concepto, subtotal, igv, total, moneda);           
+        
+        oVentas = oAdmVentas.Fn_buscar_nro_documento(numero);        
+        
+        assertEquals(fecha_emision, oVentas.getFecha_emision());
+        assertEquals(fecha_vencimiento, oVentas.getFecha_vencimiento());
+        assertEquals(empresa, oVentas.getEmpresa());
+        assertEquals(fecha_pago, oVentas.getFecha_pago());
+        assertEquals(estado, oVentas.getEstado());        
+        assertEquals(concepto, oVentas.getConcepto());
+        assertEquals(subtotal, oVentas.getSubtotal(),0);
+        assertEquals(igv, oVentas.getIgv(),0);
+        assertEquals(total, oVentas.getTotal(),0);
+        assertEquals(moneda, oVentas.getMoneda());        
     }
+    
+//    public ArrayList<String> fn_MostrarDocumentos(String numero) {
+//
+//        ArrayList<String> objlistanombre = new ArrayList<String>();
+//
+//        //posibles nombres
+//        objlistanombre.add(S_nombre + "_Premium");
+//        objlistanombre.add(S_nombre + "_Estelar");
+//        objlistanombre.add(S_nombre + "_Residencial");
+//        objlistanombre.add(S_nombre + "_Exclusivo");
+//        objlistanombre.add(S_nombre + "_Primera Clase");
+//
+//        for (String nombrealternativo : objlistanombre) {
+//
+//            if (Fn_buscarnombre(nombrealternativo) != null) {
+//
+//                objlistanombre.remove(nombrealternativo);
+//            }
+//        }
+//
+//        if (objlistanombre.size() < 5) {
+//
+//
+//            for (int i = 0; i < 5 - objlistanombre.size(); i++) {
+//
+//                int x = ((int) Math.random() * 100);
+//                objlistanombre.add(S_nombre + "_" + Integer.toString(x));
+//            }
+//
+//
+//        }
+//
+//        return objlistanombre;
+//    }
 }
